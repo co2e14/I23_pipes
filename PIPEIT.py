8ae1b73f178677b08d71bdfc38f92cc983361584
co2e14 <christian.orr@diamond.ac.uk>
2020-07-23 14:36:18 +0100

    working update

--- PIPEIT.py
+++ PIPEIT.py
@@ -5,28 +5,48 @@
 """
 
 import os
-
+from subprocess import PIPE, run
 
 os.system("module load global/cluster")
 os.system("module load dials")
-os.system("module load autoPROC")
+
+
+def out(command):
+    result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True,
+                 shell=True)
+    return result.stdout
+    print(result)
+
 
 resolution = str(0.1)
 
+workingdir = os.getcwd()
 dataloc = input("Base path to data: ")
-xia_andor_autoPROC = str(input("Do you want to run (x)ia2, (a)utoPROC or (b)oth? ").lower)
-os.system("cd "+dataloc+"; module load autoPROC; find_images -L -r")
-
+xia_andor_autoPROC = str(
+        input("Do you want to run (x)ia2, (a)utoPROC or (b)oth? ").lower)
 
 print("Creating subfolders...")
 if xia_andor_autoPROC == "x" or "b":
     if not os.path.exists("xia2_dials"):
-        os.system("mkdir xia2_dials")
+        os.mkdir("xia2_dials")
 if xia_andor_autoPROC == "x" or "b":
     if not os.path.exists("xia2_xds"):
-        os.system("mkdir xia2_xds")
+        os.mkdir("xia2_xds")
 if xia_andor_autoPROC == "a" or "b":
     if not os.path.exists("autoPROC"):
-        os.system("mkdir autoPROC")
+        os.mkdir("autoPROC")
 print("Done")
 
+os.chdir(dataloc)
+
+here = os.popen("module load autoPROC; find_images -L -r").read()
+print(here)
+os.chdir(os.path.join(workingdir, "autoPROC"))
+
+# =============================================================================
+# with open 
+#     for line in here:
+#         if line.startswith("  -Id ""):
+#             output
+# 
+# =============================================================================
